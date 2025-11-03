f11f59b877acc31ae19a4b76a55756693de883ee
raiven.rt <raiven.rt@gmail.com>
2025-11-03 15:05:40 +0200

    feat: add gameboard with players details

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 function App() {
-  return <>TIC-TAC-TOE</>;
+  return (
+    <main>
+      <section id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">John</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Doe</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        <section>GAME BOARD</section>
+      </section>
+      <section>LOG</section>
+    </main>
+  );
 }
 
 export default App;
