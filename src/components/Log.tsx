701113f9f777be1aee5c3c3733f7aded9b593177
rts <raiven.rt@gmail.com>
2025-11-04 14:38:11 +0200

    feat: add Log.tsx component logic

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -1,7 +1,15 @@
 import type { GameTurn } from "../hooks/useGameTurns";
 
 const Log = ({ turns }: { turns: GameTurn[] }) => {
-  return <ol id="log">Log</ol>;
+  return (
+    <ol id="log">
+      {turns.slice(0, 5).map((turn) => (
+        <li key={`${turn.row}${turn.col}`}>
+          {turn.playerSymbol} at {turn.row}, {turn.col}
+        </li>
+      ))}
+    </ol>
+  );
 };
 
 export default Log;
